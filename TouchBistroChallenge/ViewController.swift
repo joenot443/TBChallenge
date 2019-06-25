1ea68a1168d2b7d2d484f26fbaaa2e0368d374d5
Joseph Crozier <joe.crozier@live.ca>
2019-06-25 17:35:35 -0400

    Initial Commit

---
+++ TouchBistroChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchBistroChallenge
+//
+//  Created by Joseph Crozier on 2019-06-25.
+//  Copyright © 2019 Joe Crozier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
